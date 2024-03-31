762e7b9c1b8e7fb0a9052495fcfed71691155a1c
Ahsanawan123 <awan.ahsan15d@gmail.com>
2024-03-31 17:54:39 -0500

    Ahsan Awan: Updated Choose Tester/DeveloperPage
    - Change themes, styling and position of the "Select Account Type, Tester, Developer, submit", change card theme, color, border and shadow.

--- app/Choose/page.tsx
+++ app/Choose/page.tsx
@@ -39,14 +39,14 @@ const Choose = () => {
     <>
      <div className="min-h-screen flex justify-center items-center bg-gradient-to-r from-black via-Fuchsia-900 to-Purple-950">
      
-      <div className="card lg:card-side text-black bg-blue-100 w-1/3 h-auto shadow-xl shadow-cyan-300">
- 
+      <div className="card lg:card-side text-Sky-100 bg-gray-800 w-1/3 h-auto shadow-2xl shadow-cyan-400 rounded-lg border border-cyan-400 hover:shadow-xl transition-shadow duration-300"
+           style={{ boxShadow: '0 10px 25px -5px rgba(6, 182, 212, 0.5)' }}>
         <figure>
         </figure>
-        <div className="card-body my-20">
-          <h1 className="card-title text-3xl items-center">Select Account Type</h1>
+        <div className="card-body mt-20 mb-4 flex flex-col items-center">
+          <h1 className="card-title text-3xl mt-[-80px]">Select Account Type</h1>
           <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-baseline">
-            <div className="form-control py-4">
+            <div className="form-control py-2">
               <label className="label cursor-pointer flex items-center gap-2">
                 <input
                   type="radio"
@@ -55,10 +55,10 @@ const Choose = () => {
                   value="Tester"
                   {...register('role')}
                 />
-                <span className="label-text text-black text-xl">Tester</span>
+                <span className="label-text text-Sky-100 text-xl">Tester</span>
               </label>
             </div>
-            <div className="form-control py-4">
+            <div className="form-control py-2">
               <label className="label cursor-pointer flex items-center gap-2">
                 <input
                   type="radio"
@@ -66,10 +66,10 @@ const Choose = () => {
                   value="Developer"
                   {...register('role')}
                 />
-                <span className="label-text text-black text-xl">Developer</span>
+                <span className="label-text text-Sky-100 text-xl">Developer</span>
               </label>
             </div>
-            <button className="btn btn-primary mt-4" type="submit">
+            <button className="btn btn-primary mx-auto block mt-12" type="submit">
               Submit
             </button>
           </form>
